8ac9e63e877937b92ab834e00eefba771d00de20
su-bham <reachoutshubham@gmail.com>
2025-01-28 17:49:22 +0530

    Last message change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Git and Github
+          Learn React with Git and Github and Computer
         </a>
       </header>
     </div>
